46d2b20309a1993557091ad57c23541896d3f345
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-02-17 12:14:46 -0800

    Completed selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,7 +2,20 @@
 public class SelectionSort implements SortingAlgorithm {
 	
 	public void sort(int [] a) {
-		
+		for (int i = 0; i < a.length; i++) {
+			
+			int index = i;
+			int min = a[i];
+			
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] < min) {
+					min = a[j];
+					index = j;
+				}
+			}
+			
+			a[index] = a[i];
+			a[i] = min;		
+		}
 	}
-
 }
