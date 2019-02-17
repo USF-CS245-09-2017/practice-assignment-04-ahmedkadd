c8337f05c6a1b8a8fd39b263c322df2f65ceefb5
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-02-17 11:48:03 -0800

    Completed algorithm

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,8 +1,19 @@
-
 public class InsertionSort implements SortingAlgorithm {
 	
 	public void sort(int [] a) {
-		
+		for (int i = 1; i < a.length; i++) {
+			int num = a[i];
+			
+			if (num < a[i - 1]) {
+				int j = i - 1;
+				
+				while (j >= 0 && num < a[j]) {
+					a[j + 1] = a[j];
+					j--;
+				}	
+				
+				a[j + 1] = num;
+			}
+		}
 	}
-
 }
