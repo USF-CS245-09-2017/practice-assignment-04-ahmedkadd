04dc7822a4a83c8e2d59cdfa6d4219ef38856b97
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-02-18 15:37:20 -0800

    Optimized algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,20 +2,19 @@
 public class SelectionSort implements SortingAlgorithm {
 	
 	public void sort(int [] a) {
-		for (int i = 0; i < a.length; i++) {
-			
-			int index = i;
-			int min = a[i];
+		for (int i = 0; i < a.length - 1; i++) {
+	
+			int min_index = i;
 			
 			for (int j = i + 1; j < a.length; j++) {
-				if (a[j] < min) {
-					min = a[j];
-					index = j;
+				if (a[j] < a[min_index]) {
+					min_index = j;
 				}
 			}
 			
-			a[index] = a[i];
-			a[i] = min;		
+			int min_temp = a[min_index];
+			a[min_index] = a[i];
+			a[i] = min_temp;		
 		}
 	}
 }
