9b3cd7c61a6fa84ec8bb217985611b3e49a36885
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-02-21 18:25:13 -0800

    Comments

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort implements SortingAlgorithm {
 		for (int i = 0; i < n - 1; i++) {
 			swapped = false;
 			
-			for (int j = 1; j < n - i; i++) {
+			for (int j = 1; j < n - i; j++) {
 				if (a[j - 1] > a[j]) {
 					temp = a[j - 1];
 					a[j - 1] = a[j];
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,3 @@
-
 public class SelectionSort implements SortingAlgorithm {
 	
 	public void sort(int [] a) {
